2402af00640c445c270d746c913f304b48885615
skyr3x <43004098+DatSkyr3x@users.noreply.github.com>
2025-02-05 22:40:53 +0100

    update 5 files and create 2 files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# gameboy emulator
+# webfishing GB integration
+uses serverboy on a websocket backend to send screen data to WEBFISHING which renders it using chalk canvases and sends back input data to the emulator
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Gameboy from "serverboy";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import "ws";
 import * as path from "path";
+import { PNG } from "pngjs";
 
 const gameboy = new Gameboy();
 const romName = "pokecrystal.gbc"
@@ -10,8 +11,29 @@ const rom = readFileSync(romPath)
 
 const WIDTH = 160, HEIGHT = 144
 
+function splitArray<T>(array: T[], chunkSize: number): T[][] {
+	const chunks: T[][] = [];
+	for (let i = 0; i < array.length; i += chunkSize) {
+		chunks.push(array.slice(i, i + chunkSize));
+	}
+	return chunks;
+}
+
 gameboy.loadRom(rom)
 
-gameboy.doFrame()
-var screencap = gameboy.getScreen()
-console.log(screencap);
+for (let i=0; i < 2000; i++) {
+	gameboy.doFrame()
+}
+//gameboy.pressKey(Gameboy.KEYMAP.START)
+
+var pixels = gameboy.getScreen()
+var splitPixels = splitArray(pixels, 4)
+console.log(splitPixels[splitPixels.length - 1])
+
+var png = new PNG({ width: 160, height: 144 });
+for (let i=0; i<pixels.length; i++) {
+   png.data[i] = pixels[i];
+}
+
+var buffer = PNG.sync.write(png);
+writeFileSync('out.png', buffer);
